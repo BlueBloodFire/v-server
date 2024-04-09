0f2ca58834430369e3301ec42e372e5ad28bca46
wangjin <wj40483471@163.com>
2024-04-10 00:10:17 +0800

    初始后端模板

---
+++ src/main/java/org/wangjin/vserver/VServerApplication.java
@@ -0,0 +1,13 @@
+package org.wangjin.vserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wangjin/vserver/VServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wangjin.vserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
